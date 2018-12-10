ace73b9065a6fcf02031e20d91e9f4a34b952610
kaneshige_yuya <miki.10pro@gmail.com>
2018-12-10 11:26:59 +0900

    modify to detect green

--- detect_coc.py
+++ detect_coc.py
@@ -5,35 +5,28 @@ import cv2
 import numpy as np
 import sys
 argv = sys.argv
-
-template = cv2.imread('data/bug_template.jpg')  # テンプレート画像
- 
-def red_filter(image):
-    image = image.astype(np.float32)
-    image3 = image[:,:,1]
-    image3 /= 255
-    return image3
     
 def calcCircleLevel (contour, area):
     perimeter = cv2.arcLength(contour, True)
     circle_level = 4.0 * np.pi * area / (perimeter * perimeter); # perimeter = 0 のとき気をつける
     return circle_level
 
-def detect_coc(image, template):
+def detect_coc(image):
     #image = cv2.resize(image, (int(image.shape[1]/2), int(image.shape[0]/2)))
     centers = []
-    copy = image.copy()    
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    image = image[:,:,1]
-    image = image*red_filter(copy)
-    image = image.astype(np.uint8)
-    
+    lower = np.array([50, 200, 0])
+    upper = np.array([100, 255, 250])
+    mask = cv2.inRange(image, lower, upper)
+    image = cv2.bitwise_and(image, image, mask=mask)
+    print(image.shape)
+    image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ret,image = cv2.threshold(image,50,255,cv2.THRESH_TOZERO)
     kernel_gauss = np.ones((3,3), np.float32)/9
-    kernel = np.array([[1,1,1],[1,-8,1],[1,1,1]], np.float32)
-    image = cv2.filter2D(image, -1, kernel)
-    image = cv2.filter2D(image, -1, kernel_gauss)
-    image = cv2.dilate(image, kernel, iterations = 3)
+    #image = cv2.filter2D(image, -1, kernel_gauss)
+    #image = cv2.dilate(image, kernel_gauss, iterations = 3)
+    cv2.imwrite('data/mono.jpg', image)
     image, contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
     if len(contours) >= 1:
@@ -41,25 +34,21 @@ def detect_coc(image, template):
             (x,y),radius = cv2.minEnclosingCircle(cont)
             center = (int(x),int(y))
             radius = int(radius)
-            rect = cv2.minAreaRect(cont)
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
-            if radius >= 13 and radius <= 20 and cv2.contourArea(cont)/(3.14*radius**2) > 0.55:
-                image = cv2.drawContours(image,[box],0,(255,255,255),2)
-            
+            if radius > 20 and radius < 100:
                 image = cv2.circle(image,center,radius,(255,255,255),2)
                 centers.append([int(x), int(y)])
-    cv2.imwrite(argv[2] + ".jpg", image)
+                print(radius)
+    cv2.imwrite("data/result2.jpg", image)
     return centers
     
-file_dir = './data/'
+file_dir = 'data/'
 
 def main_process(path):
     image = cv2.imread(path)
-    return detect_coc(image, template)
+    return detect_coc(image)
 
 if __name__ == '__main__':
-    if len(argv) != 3:
+    if len(argv) != 2:
         print("usage:python3 detect_coc.py file_name")
     else:
         print(main_process(argv[1]))  
