7cd0394cccee2e02e9b7aed9fe945597ed1e5761
godiva-frappuccino <miki.10pro@gmail.com>
2018-12-17 15:47:58 +0900

    modify

--- Godiva_AI.py
+++ Godiva_AI.py
@@ -12,7 +12,8 @@ rage_word = ['馬鹿', 'マヌケ', 'アホ']
 stop_word = ['ごめん', 'すみません']
 apolo_word = ['ゆるして']
 words = ['', '', '', '', '']
-laugh = "uhuhu.wav"
+#laugh = "uhuhu.wav"
+laugh = "rage.wav"
 okotta = "rage.wav"
 mukka = "mukka.wav"
 atack = "atack.wav"
@@ -23,47 +24,48 @@ class Terminator(mk.Mumeikaneshige):
     def __init__(self):
         super().__init__()
 
-    def say(voice):
-        self.controllers['JTalk'].cmd_queue.put(voice)
+    def say(self, voice):
+        self.controllers['JTalk'].cmd_queue.put(laugh)
 
-    def apologize():
+    def apologize(self):
       voice = self.senders['Julius'].msg_queue.get()
       if voice in stop_word:
+          print("I was apologized")
           self.say(gomen)
           return True
       elif voice in apolo_word:
+          print("not aologized")
           self.say(mukka)
           return False
       else:
+          print("not aologized")
           self.say(mukka)
           return False
 
-    def get_rotate_angle(image, rocs, gakaku = 60):
-        roc = rocs[0]
+    def get_rotate_angle(self, image, roc, gakaku = 60):
         center = int(image.shape[1]/2)
         # 右を正とする
         return (roc - center) / gakaku
 
-    def go_straight(speed=30000):
+    def go_straight(self):
+        speed = 30000
         self.controllers['Motor'].cmd_queue.put((speed, speed))
         while True:
-            if self.apologize():
-                return False
-            else:
-                self.say(okotta)
             dst_msg = self.senders['SRF02'].msg_queue.get()
-            if dst_msg[0] < 15 or dst_msg[1] < 15:
-                self.controllers['Motor'].cmd_queue.put((e, e))
+            print(dst_msg)
+            if dst_msg[0] < 20 or dst_msg[1] < 20:
+                self.controllers['Motor'].cmd_queue.put((0, 0))
                 return True
 
    
-    def rotate_by_angle(speed=10000, angle):
+    def rotate_by_angle(self, angle):
+        speed = 10000
         t = angle/40*3 + 1/2
         self.controllers['Motor'].cmd_queue.put((speed, -speed))
         time.sleep(t)
         self.controllers['Motor'].cmd_queue.put((0, 0))
     
-    def smash():
+    def smash(self):
         self.controllers['Arm'].cmd_queue.put(60)
         time.sleep(1)
         self.say(atack)
@@ -72,44 +74,47 @@ class Terminator(mk.Mumeikaneshige):
         self.controllers['Arm'].cmd_queue.put(60)
        
         
-    def rotate_nine(right = True):
+    def rotate_nine(self, right = True):
         speed =30000 if right else -30000
-        self.controllers['Moror'].cmd_queue.put((speed, -speed))
+        self.controllers['Motor'].cmd_queue.put((speed, -speed))
         time.sleep(5)
-        self.controllers['Moror'].cmd_queue.put((speed, -speed))
+        self.controllers['Motor'].cmd_queue.put((speed, -speed))
           
     def rage(self, rotate_rate = 90):
         find = False
         
         # start rage_mode
         print("Rage Mode...")
-        self.say(okotta)
         self.smash()
         
         # search human to smash
         for i in range(int(360 / rotate_rate)):
-            # if apologized finish
-            if self.apologize():
-                break
-            print("rotate to find human")
-            self.rotate_nine(right = False)
+            print("rotate to find human", i)
+            self.rotate_nine(right = True)
             print("stop and find human")
             frame1, frame2 = self.senders['Webcamera'].msg_queue.get()
             roc1 = human_detect(frame1)
             roc2 = human_detect(frame2)
+            # if apologized finish
+            if self.apologize():
+                break
             
             # if found human
             if len(roc2) != 0:
                 print("I found human!")
-                x_center = (roc2[0][0] + roc2[0][2]) / 2
+                print("tuple", roc2)
+                x_center = (roc2[0][0][0] + roc2[0][1][0]) / 2
                 self.say(okotta)
+                print("adjust angle to smash")
                 self.rotate_by_angle(self.get_rotate_angle(frame2, x_center))
                 find = True
                 break
             elif len(roc1) != 0:
                 print("I found human!!!")
-                x_center = (roc1[0][0] + roc1[0][2]) / 2
+                print("tuple", roc1)
+                x_center = (roc1[0][0][0] + roc1[0][1][0]) / 2
                 self.say(okotta)
+                print("adjust angle to smash")
                 self.rotate_by_angle(self.get_rotate_angle(frame1, x_center))
                 find = True
                 break
@@ -120,14 +125,15 @@ class Terminator(mk.Mumeikaneshige):
                 self.say(mukka)
         
         # found! let's smash human!
+        print("let's smash human")
         if find:
-            self.say(mukka)
             if self.go_straight():
                 self.smash()
                 
                 # everything is over, apologize
                 time.sleep(2)
                 self.say(gomen)
+            find = False
         
         # couldn't find, akirame...        
         else:
